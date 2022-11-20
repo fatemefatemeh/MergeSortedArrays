346686ff7a3e04597c2ebba43fcf868052a396ba
Fatemeh <fatemeh.khaksar8@gmail.com>
2022-11-20 16:16:12 +0330

    edited the file .add return to it.

--- Merge-Sorted-Arrays.py
+++ Merge-Sorted-Arrays.py
@@ -7,21 +7,29 @@ class Solution(object):
         :type nums2: List[int]
         :type n: int
         """
-        i=int(m-1)
-        j=int(n-1)
-        k=int(m+n-1)
-
-        while k>=0:
+        i = int(m - 1)
+        j = int(n - 1)
+        k = int(m + n - 1)
+        
+        while (k >= 0):
             if i>=0 and j>=0:
-                if nums1[i]>nums2[j]:
-                    nums1[k]=nums2[j]
-                    j=j-1
-                    k=k-1
+                print(nums1[i])
+                print(nums2[j])
+                if(nums2[j] > nums1[i]):
+                    nums1[k] = nums2[j]
+                    k -= 1
+                    j -= 1
                 else:
-                    nums1[k]=nums1[i]
-                    i=i-1
-                    k=k-1
-                if j>=0:
-                    nums1[k]=nums2[j]
-                else:
-                    break
+                    nums1[k] = nums1[i]
+                    k -= 1
+                    i -= 1
+            elif j >= 0:
+                nums1[k] = nums2[j]
+                k -= 1
+                j -= 1
+            else:
+                break
+        return nums1
+
+s = Solution()
+print(s.merge(nums1=[1, 2, 3, 0, 0, 0], nums2=[2, 5, 6], m=3, n=3))
